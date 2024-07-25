b66870128291fa1c8940c35e7105294b40edf46a
luuminhnhat12 <nguoidep433@gmail.com>
2024-07-25 19:00:03 +0700

    dashboard

---
+++ app/src/main/java/com/example/recipe_application/view/ScreenDashboard.kt
@@ -0,0 +1,12 @@
+package com.example.recipe_application.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recipe_application.R
+
+class ScreenDashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_dashboard)
+    }
+}
